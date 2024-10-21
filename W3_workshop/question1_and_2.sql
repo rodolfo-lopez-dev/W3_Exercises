9b87b5e2e32c4457b0c1725e12c972d4ff07d396
Rodolfo <rlopez1@appdev.yearup.org>
2024-10-20 19:03:25 -0500

    added additional answers

--- W3_workshop/question1_and_2.sql
+++ W3_workshop/question1_and_2.sql
@@ -18,8 +18,6 @@ ORDER BY last_name;
  WHERE actor.first_name = 'Joe';
  
  
- # 
- 
  SELECT first_name, last_name
  FROM actor 
  WHERE last_name LIKE '%GEN%';
@@ -52,4 +50,46 @@ ORDER BY last_name;
  SELECT first_name, last_name
  FROM actor WHERE last_name = 'Williams';
  
- 
+ SELECT staff.first_name, staff.last_name, SUM(payment.amount) AS "Total Amount"
+FROM staff
+INNER JOIN payment ON staff.staff_id = payment.staff_id
+WHERE payment.payment_date BETWEEN '2005-08-01' AND '2005-08-31'
+GROUP BY staff.staff_id;
+
+SELECT film.title, COUNT(film_actor.actor_id) AS "Number of Actors" FROM film
+INNER JOIN film_actor ON film.film_id = film_actor.film_id
+GROUP BY film.film_id;
+
+SELECT COUNT(*) FROM inventory
+INNER JOIN film ON inventory.film_id = film.film_id
+WHERE film.title = 'HUNCHBACK IMPOSSIBLE';
+
+SELECT title FROM film
+WHERE (title LIKE 'K%' OR title LIKE 'Q%')
+AND language_id = (
+    SELECT language_id
+    FROM language
+    WHERE name = 'English'
+);
+
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id)
+VALUES (
+    '2024-10-10 10:00:00', 
+    (SELECT inventory_id FROM inventory
+     WHERE film_id = (SELECT film_id FROM film WHERE title = 'ACADEMY DINOSAUR') 
+     AND store_id = 1 LIMIT 1),
+    (SELECT customer_id FROM customer WHERE first_name = 'Mary' AND last_name = 'Smith'),
+    '2024-10-17 10:00:00', 
+    (SELECT staff_id FROM staff WHERE first_name = 'Mike' AND last_name = 'Hillyer')
+);
+
+SELECT * 
+FROM rental
+WHERE customer_id = (SELECT customer_id FROM customer WHERE first_name = 'Mary' AND last_name = 'Smith')
+ORDER BY rental_date DESC
+LIMIT 1;
+
+
+
+
+
